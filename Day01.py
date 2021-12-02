8cd81191b9529ddbe1c3e566ee1b3f20564e25a8
Márton <marton.metzing@gmail.com>
2021-12-02 23:07:54 +0100

    Day 1 Star 1

--- Day01.py
+++ Day01.py
@@ -1,2 +1,17 @@
-msg = "Hello World!"
-print(msg)
+def _parse(line : str):
+    return int(line.strip())
+
+file = open("Day01.txt", "r")
+
+increases = 0
+
+depths = list(map(_parse, file.readlines()))
+
+for index in range(1, len(depths)):
+    last = depths[index - 1]
+    current = depths[index]
+
+    if (last < current):
+        increases = increases + 1
+
+print(increases)
