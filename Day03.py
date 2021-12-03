94debd3b8d791053f6b8679466eb54ca5fea3733
Márton <marton.metzing@gmail.com>
2021-12-03 08:09:56 +0100

    Day 3 Star 2

--- Day03.py
+++ Day03.py
@@ -1,32 +1,69 @@
+def _digitAtIndexEquals(digit : str, index : int):
+
+    def _result(line : str):
+        return line[index] == digit
+    
+    return _result
+
 file = open("Day03.txt", "r")
 
 lines = file.readlines()
 
 length = len(lines[0])
 
-gamma = ""
-epsilon = ""
+# finding oxygen value
 
-for i in range(0, length - 1):
+i = 0
+oxygenCandidateLines = lines
+
+while len(oxygenCandidateLines) > 1:
 
     ones = 0
     zeroes = 0
 
-    for line in lines:
+    for line in oxygenCandidateLines:
         if line[i] == '0':
             zeroes += 1
         else:
             ones += 1
 
     if ones > zeroes:
-        gamma += '1'
-        epsilon += '0'
+        oxygenCandidateLines = list(filter(_digitAtIndexEquals("1", i), oxygenCandidateLines))
+    elif ones < zeroes:
+        oxygenCandidateLines = list(filter(_digitAtIndexEquals("0", i), oxygenCandidateLines))
     else:
-        gamma += '0'
-        epsilon += '1'
+        oxygenCandidateLines = list(filter(_digitAtIndexEquals("1", i), oxygenCandidateLines))
 
-gammaParsed = int(gamma, 2)
-epsilonParsed = int(epsilon, 2)
+    i += 1 
+        
+oxygen = int(oxygenCandidateLines[0], 2)
 
-print(gammaParsed * epsilonParsed)
+# finding co2 value
+
+co2CandidateLines = lines
+i = 0
+
+while len(co2CandidateLines) > 1:
+
+    ones = 0
+    zeroes = 0
+
+    for line in co2CandidateLines:
+        if line[i] == '0':
+            zeroes += 1
+        else:
+            ones += 1
+
+    if ones > zeroes:
+        co2CandidateLines = list(filter(_digitAtIndexEquals("0", i), co2CandidateLines))
+    elif ones < zeroes:
+        co2CandidateLines = list(filter(_digitAtIndexEquals("1", i), co2CandidateLines))
+    else:
+        co2CandidateLines = list(filter(_digitAtIndexEquals("0", i), co2CandidateLines))
+
+    i += 1 
+
+co2 = int (co2CandidateLines[0], 2)
+
+print(oxygen * co2)
 
