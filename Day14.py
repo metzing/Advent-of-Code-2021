1e182e348530251cb104be76a102e16a964ff2f9
Márton Metzing <marton.metzing@gmail.com>
2021-12-14 10:57:23 +0100

    Day 14 Star 2

--- Day14.py
+++ Day14.py
@@ -10,29 +10,53 @@ while line := file.readline().strip():
     split = line.split(" -> ")
     insertionrules[split[0]] = split[1]
 
-for _ in range(10):
+replacementPairs = {}
 
-    newTemplate = ""
+for pair in insertionrules.keys():
+    replacementPairs[pair] = [ pair[0] + insertionrules[pair], insertionrules[pair] + pair[1] ]
 
-    for i in range(len(template) - 1):
+currentPairs = {}
 
-        inserted = insertionrules[template[i] + template[i + 1]]
+for i in range(len(template) - 1):
 
-        newTemplate += template[i] + inserted
-
-    newTemplate += template[len(template) - 1]
-    template = newTemplate
+    pair = template[i] + template[i + 1]
     
+    if pair not in currentPairs.keys():
+        currentPairs[pair] = 0
+
+    currentPairs[pair] += 1
+
+for _ in range(40):
+
+    nextPairs = {}
+
+    for pair in currentPairs.keys():
+
+        replacements = replacementPairs[pair]
+
+        if replacements[0] not in nextPairs.keys():
+            nextPairs[replacements[0]] = 0
+
+        if replacements[1] not in nextPairs.keys():
+            nextPairs[replacements[1]] = 0
+
+        nextPairs[replacements[0]] += currentPairs[pair]
+        nextPairs[replacements[1]] += currentPairs[pair]
+
+    currentPairs = nextPairs
+
 count = {}
 
-for char in template:
+for pair in currentPairs.keys():
     
-    if char not in count.keys():
-        count[char] = 0
-    
-    count[char] += 1
+    for char in pair:
 
-sorted = sorted(count.values())
-print(count)
+        if char not in count.keys():
+            count[char] = 0
+    
+        count[char] += currentPairs[pair]
+
+sorted = sorted(round(value / 2) for value in count.values())
+
 print(next(iter(reversed(sorted))) - next(iter(sorted)))
 
